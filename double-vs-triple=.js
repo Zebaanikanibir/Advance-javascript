cbaf22058292e01f37cf4ec20a9f028e71576939
Zebaccanikanibir <zanikanibir@gmail.com>
2021-02-02 00:56:04 +0600

    double vs triple

---
+++ double-vs-triple=.js
@@ -0,0 +1,19 @@
+const first = 2;
+const second ="2";
+if (first == second) {
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+
+}
+//important interview question
+//double equal  convert two type value same
+//triple equal judge the condition clearly.it check value and type
+const first1 = 2;
+const second1 ="2";
+if (first1 === second1) {
+    console.log("condition is true")
+}else{
+    console.log("condition is false")
+
+}
